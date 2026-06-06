805419f17ffabe59b7d1166f5dbcb084b8b39ad9
Juraj Kvasnak <kvasnakjuraj2112@gmail.com>
2026-06-06 19:35:24 +0200

    Priadnie servera a clienta 2#

--- test_client.py
+++
@@ -1,9 +0,0 @@
-from client import Network
-
-print("Pokúšam sa pripojiť na server...")
-n = Network("127.0.0.1")
-
-if n.player_id is not None:
-    print(f"ÚSPECH! Pripojený k serveru. Moje pridelené ID je: {n.player_id}")
-else:
-    print("CHYBA: Nepodarilo sa pripojiť.")
